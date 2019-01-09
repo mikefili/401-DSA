3697d7a1088a8761787473761849b239b8666acb
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-09 09:43:40 -0800

    repo, sln, and xunit test scaffold built

---
+++ Challenges/ArrayInsert/UnitTest1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using ArrayShift;
+
+namespace UnitTest1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
