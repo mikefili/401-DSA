010976b295b2d7016a1e1421ed19e81d1cadffde
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-14 09:57:23 -0800

    Insert, Includes, Print, & ExampleList methods work

---
+++ Data-Structures/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using Linked_List.classes;
+using System;
+
+namespace Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExampleList();
+        }
+
+        static void ExampleList()
+        {
+            LinkedList list = new LinkedList();
+            list.Insert(5);
+            list.Insert(10);
+            list.Insert(15);
+            list.Print();
+            
+        }
+    }
+}
---
+++ Data-Structures/LinkedList/LinkedList/classes/LinkedList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List.classes
+{
+    class LinkedList
+    {
+        // Head property
+        public Node Head { get; set; }
+        public Node Current { get; set; }
+
+        public void Insert(int value)
+        {
+            Node exampleNode = new Node(value); // circle
+            exampleNode.Next = Head; // arrow
+            Head = exampleNode; // move head to newly instantiated node
+        }
+
+        public bool Includes(int value)
+        {
+            Current = Head; // reset to Head node so that we start at the front of the LL
+            while (Current.Next != null) // verify we are not at end of LL
+            {
+                if (Current.Value == value)
+                {
+                    return true;
+                }
+                Current = Current.Next;
+            }
+            if (Current.Value == value)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Print()
+        {
+            Console.Write("HEAD --> ");
+            Console.Write(Head.Value);
+            Console.Write(" --> ");
+            Node Current = Head;
+            while (Current.Next != null)
+            {
+                Current = Current.Next;
+                Console.Write(Current.Value);
+                Console.Write(" --> ");
+            }
+            Console.Write("NULL");
+            Console.ReadLine();
+        }
+    }
+}
