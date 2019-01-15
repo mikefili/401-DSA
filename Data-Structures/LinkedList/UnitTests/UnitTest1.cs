7afae6e4930f8e5a0eb4b78b5c0251e146889305
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-15 09:31:34 -0800

    CanInsertBefore test passes!

--- Data-Structures/LinkedList/UnitTests/UnitTest1.cs
+++ Data-Structures/LinkedList/UnitTests/UnitTest1.cs
@@ -8,6 +8,28 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
+        public void CanInsertBefore()
+        {
+            LinkedList list = new LinkedList();
+            list.Insert(5);
+            list.Insert(10);
+            list.Insert(15);
+            list.InsertBefore(10, 12);
+
+            int[] expectedValues = { 15, 12, 10, 5 };
+            int[] actualValues = new int[4];
+            int i = 0;
+            list.Current = list.Head;
+            while (list.Current != null)
+            {
+                actualValues[i] = list.Current.Value;
+                list.Current = list.Current.Next;
+                i++;
+            };
+            Assert.Equal(expectedValues, actualValues);
+        }
+
+        [Fact]
         public void CanInsertSingleValue()
         {
             LinkedList list = new LinkedList();
@@ -90,5 +112,18 @@ namespace UnitTests
         {
             Assert.NotNull(Program.ExampleList());
         }
+
+        //[Fact]
+        //public void CanAppendNode()
+        //{
+        //    LinkedList list = new LinkedList();
+        //    list.Insert(5);
+        //    list.Insert(10);
+        //    list.Insert(15);
+        //    list.Append(16);
+
+        //    int[] expectedValues = { 15, 10, 5, 16 };
+        //    Assert.Equal(expectedValues, this);
+        //}
     }
 }
