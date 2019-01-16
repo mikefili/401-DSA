53fac1b1a239a3aaa74760d28a579a1da40ea079
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-16 10:12:11 -0800

    algorith implemented successfully

---
+++ Challenges/KthFromEnd/ll_kth_from_end/UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Linked_List.classes;
+using ll_kth_from_end;
+using Xunit;
+
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CanFindKthNodeValue()
+        {
+
+        }
+    }
+}
---
+++ Challenges/KthFromEnd/ll_kth_from_end/ll_kth_from_end/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Linked_List.classes;
+
+namespace ll_kth_from_end
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public int KthFromEnd(int k, LinkedList linkedList)
+        {
+            int counter = 0;
+            linkedList.Current = linkedList.Head;
+            while (linkedList.Current.Next != null)
+            {
+                linkedList.Current = linkedList.Current.Next;
+                counter++;
+            }
+            linkedList.Current = linkedList.Head;
+            for (int i = 0; i < counter - k; i++)
+            {
+                linkedList.Current = linkedList.Current.Next;
+            }
+            return linkedList.Current.Value;
+        }
+    }
+}
