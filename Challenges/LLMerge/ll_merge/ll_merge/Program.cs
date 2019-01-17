5365d838ad32de9021781722ee2b6862ca069291
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-17 09:24:58 -0800

    initial setup

---
+++ Challenges/LLMerge/ll_merge/ll_merge/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Linked_List.classes;
+
+namespace ll_merge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
