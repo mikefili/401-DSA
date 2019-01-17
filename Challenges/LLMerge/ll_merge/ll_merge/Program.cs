d72194756d3f895a589c56acebc632e82d8bdf2f
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-17 10:48:58 -0800

    able to insert first node of LL2 into correct position within LL1 then cuts off remainder

--- Challenges/LLMerge/ll_merge/ll_merge/Program.cs
+++ Challenges/LLMerge/ll_merge/ll_merge/Program.cs
@@ -3,11 +3,53 @@ using Linked_List.classes;
 
 namespace ll_merge
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LinkedList linkedList1 = new LinkedList();
+            linkedList1.Insert(5);
+            linkedList1.Insert(3);
+            linkedList1.Insert(1);
+
+            LinkedList linkedList2 = new LinkedList();
+            linkedList2.Insert(6);
+            linkedList2.Insert(4);
+            linkedList2.Insert(2);
+
+            linkedList1.Print();
+            Console.WriteLine();
+            linkedList2.Print();
+            Console.WriteLine();
+            Merge(linkedList1, linkedList2);
+        }
+
+        public static void Merge(LinkedList linkedList1, LinkedList linkedList2)
+        {
+            linkedList1.Current = linkedList1.Head;
+            linkedList2.Current = linkedList2.Head;
+            Node temp = new Node(3);
+            temp = linkedList1.Head;
+
+            while (linkedList2.Current != null && temp != null)
+            {
+                temp = temp.Next;
+                linkedList1.Current.Next = linkedList2.Current;
+                linkedList2.Current = linkedList2.Current.Next;
+                linkedList1.Current = linkedList1.Current.Next;
+                linkedList1.Current = temp;
+                linkedList1.Current = linkedList1.Current.Next;
+            }          
+            if (temp != null)
+            {
+                linkedList1.Current.Next = linkedList2.Current;
+            }
+            else if (linkedList2 != null)
+            {
+                linkedList2.Current.Next = temp;
+            }
+            linkedList1.Print();
+            Console.WriteLine();
         }
     }
 }
