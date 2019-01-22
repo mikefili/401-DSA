5b08ff0ae3a9e344dfe5911ea4e63a0348a8b41a
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-21 17:12:12 -0800

    created Node and Stack classes. Created Push method.

---
+++ Data-Structures/StacksAndQueues/StacksAndQueues/StacksAndQueues/Classes/Stack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests_StacksAndQueues.Classes
+{
+    public class Stack
+    {
+        public Node Top { get; set; }
+
+        public void Push(int val)
+        {
+            Node node = new Node(val);
+            node = Top;
+        }
+    }
+}
---
+++ Data-Structures/StacksAndQueues/StacksAndQueues/UnitTests_StacksAndQueues/UnitTest1.cs
@@ -0,0 +1,15 @@
+using StacksAndQueues;
+using System;
+using Xunit;
+
+namespace UnitTests_StacksAndQueues
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
