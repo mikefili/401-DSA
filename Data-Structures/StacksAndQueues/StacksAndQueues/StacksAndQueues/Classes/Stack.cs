cde61dcbedd5d352736d85719c550b91591c3ee2
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-21 17:21:23 -0800

    Overload of Stack method completed which accepts no argument and creates an empty stack

--- Data-Structures/StacksAndQueues/StacksAndQueues/StacksAndQueues/Classes/Stack.cs
+++ Data-Structures/StacksAndQueues/StacksAndQueues/StacksAndQueues/Classes/Stack.cs
@@ -8,18 +8,37 @@ namespace UnitTests_StacksAndQueues.Classes
     {
         public Node Top { get; set; }
 
+        /// <summary>
+        /// Create a stack with at least one node in it
+        /// </summary>
+        /// <param name="node">node to be placed in stack</param>
         public Stack(Node node)
         {
             Top = node;
         }
 
+        /// <summary>
+        /// Create an empty stack
+        /// </summary>
+        public Stack()
+        {
+
+        }
+
+        /// <summary>
+        /// Create a new node with the provided value to the top of the stack
+        /// </summary>
+        /// <param name="val">value of node to be created</param>
         public void Push(int val)
         {
             Node node = new Node(val);
             node = Top;
         }
 
-        public void Pop()
+        /// <summary>
+        /// Removes a node from the top of the stack and returns its value
+        /// </summary>
+        public int Pop()
         {
 
         }
