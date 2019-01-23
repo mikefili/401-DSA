044bbb9311bf95e993d89b9a908a3cf7bf10963f
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-23 08:03:13 -0800

    set Front & Rear properties

---
+++ Challenges/QueueWithStacks/QueueWithStacks/QueueWithStacks/Classes/PseudoQueue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueWithStacks.Classes
+{
+    public class PseudoQueue
+    {
+        /// <summary>
+        /// set Front and Rear properties
+        /// </summary>
+        public Stack Front { get; set; }
+        public Stack Rear { get; set; }
+
+    }
+}
