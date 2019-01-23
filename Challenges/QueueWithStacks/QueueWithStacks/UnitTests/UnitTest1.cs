ac26c0320cd054b10b1f9986be915858f864fbb1
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-23 07:53:36 -0800

    initial setup

---
+++ Challenges/QueueWithStacks/QueueWithStacks/UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using QueueWithStacks;
+
+namespace UnitTests_StackWithQueues
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
