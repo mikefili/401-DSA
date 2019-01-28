9d7cc939798d634556f7943c99bc295c1d8adb66
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-28 09:23:04 -0800

    Node class complete & BinaryTree class created

---
+++ Data-Structures/Tree/Tree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trees.Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Data-Structures/Tree/UnitTests_Tree/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Trees.Classes;
+
+namespace UnitTests_Trees
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
