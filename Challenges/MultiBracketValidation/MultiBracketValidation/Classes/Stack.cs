765d0fac044482edde271d4650dc91a964f677f5
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-27 21:30:47 -0800

    Stack class complete

--- Challenges/MultiBracketValidation/MultiBracketValidation/Classes/Stack.cs
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Classes/Stack.cs
@@ -6,5 +6,55 @@ namespace MultiBracketValidation.Classes
 {
     class Stack
     {
+        public Node Top { get; set; }
+
+        /// <summary>
+        /// instantiate empty stack
+        /// </summary>
+        public Stack()
+        {
+
+        }
+
+        /// <summary>
+        /// instantiate stack with one node
+        /// </summary>
+        /// <param name="node">top node</param>
+        public Stack(Node node)
+        {
+            Top = node;
+        }
+
+        /// <summary>
+        /// "push" node onto the top of the stack
+        /// </summary>
+        /// <param name="value">top node value</param>
+        public void Push(char value)
+        {
+            Node node = new Node(value);
+            node.Next = Top;
+            Top = node;
+        }
+
+        /// <summary>
+        /// "pops" the top node off the stack
+        /// </summary>
+        /// <returns>top node</returns>
+        public Node Pop()
+        {
+            Node temp = Top;
+            Top = Top.Next;
+            temp.Next = null;
+            return temp;
+        }
+
+        /// <summary>
+        /// "Peeks" at the top node of the stack
+        /// </summary>
+        /// <returns>top node</returns>
+        public Node Peek()
+        {
+            return Top;
+        }
     }
 }
