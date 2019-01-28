c62f4b8d4b64fc41e681314800bf66a0a67c199f
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-27 21:53:09 -0800

    CanValidateMatchingBrackets test passes!

--- Challenges/MultiBracketValidation/UnitTests_MultiBracketValidation/UnitTest1.cs
+++ Challenges/MultiBracketValidation/UnitTests_MultiBracketValidation/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using MultiBracketValidation;
 using MultiBracketValidation.Classes;
 
 namespace UnitTests_MultiBracketValidation
@@ -7,9 +8,10 @@ namespace UnitTests_MultiBracketValidation
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanValidateMatchingBrackets()
         {
-
+            string testInput = "({[]})";
+            Assert.True(Program.MultiBracketValidation(testInput));
         }
     }
 }
