058e81dd980abfd675c3738e1be06f629e1fa328
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-28 09:49:48 -0800

    InOrder and PostOrder methods complete. BinarySearchTree class created

--- Data-Structures/Tree/Tree/Classes/BinaryTree.cs
+++ Data-Structures/Tree/Tree/Classes/BinaryTree.cs
@@ -6,5 +6,45 @@ namespace Trees.Classes
 {
     public class BinaryTree
     {
+        public Node root { get; set; }
+        public List<int> OrderedArray { get; set; }
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        public int[] PreOrder(Node root)
+        {
+            if (root != null)
+            {
+                OrderedArray.Add(root.Value);
+                PreOrder(root.LeftChild);
+                PreOrder(root.RightChild);
+            }
+            return OrderedArray.ToArray();
+        }
+
+        public int[] InOrder(Node root)
+        {
+            if (root != null)
+            {
+                PreOrder(root.LeftChild);
+                OrderedArray.Add(root.Value);
+                PreOrder(root.RightChild);
+            }
+            return OrderedArray.ToArray();
+        }
+
+        public int[] PostOrder(Node root)
+        {
+            if (root != null)
+            {
+                PreOrder(root.LeftChild);
+                PreOrder(root.RightChild);
+                OrderedArray.Add(root.Value);
+            }
+            return OrderedArray.ToArray();
+        }
     }
 }
