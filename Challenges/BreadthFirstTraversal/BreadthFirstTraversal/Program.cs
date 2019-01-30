433aefef6e029dca0aed57dfe5485f585f9f7695
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-30 11:02:20 -0800

    refactored BreadthFirst method to return an object array

--- Challenges/BreadthFirstTraversal/BreadthFirstTraversal/Program.cs
+++ Challenges/BreadthFirstTraversal/BreadthFirstTraversal/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Trees.Classes;
 
 namespace BreadthFirstTraversal
 {
@@ -6,7 +8,30 @@ namespace BreadthFirstTraversal
     {
         static void Main(string[] args)
         {
-            
+            Node root = new Node(2);
+            root.LeftChild = new Node(7);
+            root.RightChild = new Node(5);
+            root.LeftChild.LeftChild = new Node(2);
+            root.LeftChild.RightChild = new Node(6);
+            root.LeftChild.RightChild.LeftChild = new Node(5);
+            root.LeftChild.RightChild.RightChild = new Node(11);
+            root.RightChild.RightChild = new Node(9);
+            root.RightChild.RightChild.LeftChild = new Node(4);
+
+            BreadthFirst(root);
+        }
+
+        public static object[] BreadthFirst(Node root)
+        {
+            List<object> list = new List<object>();
+            Queue<Node> tree = new Queue<Node>();
+            tree.Enqueue(root);
+
+            while (tree.Count > 0)
+            {
+
+            }
+            return tree.ToArray();
         }
     }
 }
