acedf1c9d6f3d149b9922fbee120cb04f6b372f1
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-03 20:39:00 -0800

    2 bst overloads added

--- Data-Structures/Tree/Tree/Classes/BinarySearchTree.cs
+++ Data-Structures/Tree/Tree/Classes/BinarySearchTree.cs
@@ -6,6 +6,16 @@ namespace Trees.Classes
 {
     public class BinarySearchTree
     {
+        public Node Root { get; set; }
 
+        public BinarySearchTree(Node node)
+        {
+            Root = null;
+        }
+
+        public BinarySearchTree()
+        {
+
+        }
     }
 }
