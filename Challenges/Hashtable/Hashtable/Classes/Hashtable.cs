b80bef909a7f3406a64fd686c54c70e993802579
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-17 15:26:01 -0800

    basic scaffold of Add & Get methods

--- Challenges/Hashtable/Hashtable/Classes/Hashtable.cs
+++ Challenges/Hashtable/Hashtable/Classes/Hashtable.cs
@@ -6,5 +6,17 @@ namespace Hashtable.Classes
 {
     public class Hashtable
     {
+        public void Add(string key, string value)
+        {
+
+        }
+
+        public string Get(string key)
+        {
+            string value = "";
+            return value;
+        }
+
+
     }
 }
