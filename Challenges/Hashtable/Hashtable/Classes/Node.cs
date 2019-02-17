2136623bb7a4c8a1af2bf4fda64e59ded770e4c3
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-17 15:58:12 -0800

    node class set up

--- Challenges/Hashtable/Hashtable/Classes/Node.cs
+++ Challenges/Hashtable/Hashtable/Classes/Node.cs
@@ -6,5 +6,20 @@ namespace Hashtable.Classes
 {
     public class Node
     {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string KeyValuePair { get; set; }
+
+        /// <summary>
+        /// Create a new Node with a key value pair
+        /// </summary>
+        /// <param name="key">Node's key string</param>
+        /// <param name="value">Node's value string</param>
+        public Node(string key, string value)
+        {
+            Key = key;
+            Value = value;
+            KeyValuePair = $"{key}:{value}";
+        }
     }
 }
