daf20888d617f4884529c4f9ded51f4e5c3b6c3c
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-19 12:13:38 -0800

    CanFindRepeatedWordCaseSensitive & CanDetectNoRepeats tests pass. challenge complete

--- Challenges/RepeatedWord/RepeatedWord/Program.cs
+++ Challenges/RepeatedWord/RepeatedWord/Program.cs
@@ -8,9 +8,7 @@ namespace RepeatedWord
     {
         static void Main(string[] args)
         {
-            RepeatedWord("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...");
 
-            RepeatedWord("There are no repeats in this string.");
         }
 
         public static string RepeatedWord(string lengthyString)
--- Challenges/RepeatedWord/UnitTests_RepeatedWord/UnitTest1.cs
+++ Challenges/RepeatedWord/UnitTests_RepeatedWord/UnitTest1.cs
@@ -9,8 +9,24 @@ namespace UnitTests_RepeatedWord
         [Fact]
         public void CanFindRepeatedWord()
         {
+            string actual = Program.RepeatedWord("this one time there was this test I had to get to pass");
+            string expected = "this";
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CanFindRepeatedWordCaseSensitive()
+        {
             string actual = Program.RepeatedWord("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...");
             string expected = "it";
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CanDetectNoRepeats()
+        {
+            string actual = Program.RepeatedWord("There are no repeats in this string.");
+            string expected = "No Repeated Words";
             Assert.Equal(expected, actual);
         }
     }
