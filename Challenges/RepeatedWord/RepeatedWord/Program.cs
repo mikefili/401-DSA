968c87e70cc1d883596cbc14a9329fa25b5ba2b9
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-19 12:02:46 -0800

    implemented RepeatedWord method successfully

--- Challenges/RepeatedWord/RepeatedWord/Program.cs
+++ Challenges/RepeatedWord/RepeatedWord/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using HashTable;
+using HashTable.Classes;
 
 namespace RepeatedWord
 {
@@ -6,7 +8,31 @@ namespace RepeatedWord
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RepeatedWord("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...");
+
+            RepeatedWord("There are no repeats in this string.");
+        }
+
+        public static string RepeatedWord(string lengthyString)
+        {
+            Hashtable HT = new Hashtable(1024);
+
+            string[] words = lengthyString.Split(null);
+            foreach (string word in words)
+            {
+                string lowerWord = word.ToLower();
+                if (HT.Contains(lowerWord) == true)
+                {
+                    Console.WriteLine($"The Repeated Word is: {lowerWord}");
+                    return lowerWord;
+                }
+                else
+                {
+                    HT.Add(lowerWord, lowerWord);
+                }
+            }
+            Console.WriteLine("No Repeated Words");
+            return "No Repeated Words";
         }
     }
 }
