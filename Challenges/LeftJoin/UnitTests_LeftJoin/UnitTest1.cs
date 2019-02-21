a40deb56ed578ac6e34769837001e118ad00ce3f
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-21 08:10:35 -0800

    Initial setup & README

---
+++ Challenges/LeftJoin/UnitTests_LeftJoin/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using HashTable.Classes;
+using LeftJoin;
+
+namespace UnitTests_LeftJoin
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
