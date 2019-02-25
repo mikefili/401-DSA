952c9801f5c054635fc04f60fece8670360feb11
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-24 17:19:24 -0800

    initial setup

---
+++ Challenges/EC2-UniqueCharacterValidator/EC2-UniqueCharacterValidator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EC2_UniqueCharacterValidator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Challenges/EC2-UniqueCharacterValidator/UnitTests_EC2/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using EC2_UniqueCharacterValidator;
+
+namespace UnitTests_EC2
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
