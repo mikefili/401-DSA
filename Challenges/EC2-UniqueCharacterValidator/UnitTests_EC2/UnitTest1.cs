d839efa0bdf12f05f0d260bbe04e2a117134395a
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-24 18:03:58 -0800

    CanValidateStringUniqueWithSpaces test passes

--- Challenges/EC2-UniqueCharacterValidator/UnitTests_EC2/UnitTest1.cs
+++ Challenges/EC2-UniqueCharacterValidator/UnitTests_EC2/UnitTest1.cs
@@ -23,5 +23,11 @@ namespace UnitTests_EC2
         {
             Assert.False(Program.UniqueCharValidator("this will fail"));
         }
+
+        [Fact]
+        public void CanValidateStringUniqueWithSpaces()
+        {
+            Assert.True(Program.UniqueCharValidator("a b c d e f g h i j k l m n o p q r s t u v w x y z"));
+        }
     }
 }
