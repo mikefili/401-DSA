f8e89d993780cce0fc28b446d16af380a5ddac8e
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-24 18:07:08 -0800

    CanValidateAnyCharacter test passes

--- Challenges/EC2-UniqueCharacterValidator/UnitTests_EC2/UnitTest1.cs
+++ Challenges/EC2-UniqueCharacterValidator/UnitTests_EC2/UnitTest1.cs
@@ -41,5 +41,11 @@ namespace UnitTests_EC2
         {
             Assert.True(Program.UniqueCharValidator("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z"));
         }
+
+        [Fact]
+        public void CanValidateAnyCharacter()
+        {
+            Assert.True(Program.UniqueCharValidator("#$%&\'()*+,-./0123456789:;<=>?ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"));
+        }
     }
 }
